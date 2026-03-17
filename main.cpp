1ad616420103e70fead71c7c7909590df303237c
123marek321 <marekstro123@gmail.com>
2026-03-17 17:16:25 +0000

    rewrote main

--- main.cpp
+++ main.cpp
@@ -1,63 +1,59 @@
-﻿#include <iostream>
+﻿
+#include <iostream>
 using namespace std;
-
-//calculates rewards for legs
-//
-//
-//
-void calculateLegs() {
-	//Q=quantity, ZL=money XP=experience TP=trade points REP= reputation, Time = quest refresh in hours
-	float legsQ{ 100 }, legsZL{ 45000 }, legsXP{ 32000 }, legsTP{ 20 }, legsREP{ 50 }, legsTIME{ 16 };
-	cout << "How many legs did you buy: ";
-	float inpQ{}, inpZL{};
-	cin >> inpQ;
-	cout << "How much money for 1: ";
-	cin >> inpZL;
-	// start gross rewards
-	cout << "-----------------------------\n";
-	cout << "You will get the following \n";
-	cout << "Money: " << ((legsZL / legsQ) * inpQ) - inpZL * inpQ << " rubles" << '\n'; // net profit using inpZL
-	cout << "Experience: " << (legsXP / legsQ) * inpQ << '\n';
-	cout << "Trade points: " << (legsTP / legsQ) * inpQ << '\n';
-	cout << "Reputation: " << (legsREP / legsQ) * inpQ << '\n';
-	// end gross rewards
-	// old tool calculation
-	int tools = ((legsTP / legsQ) * inpQ) / 200;
-	cout << "You can get " << tools << " old tools" << '\n';
-}
-
-
-
-
+//X=quantity, ZL=money XP=experience TP=trade points REP= reputation, Time = quest refresh in hours
 
 
 
 
 void main() {
-	//selection of resource
-	cout << "Welcome to MCN Calculator: \n";
+
+	long legsZL{ 45000 }, legsXP{ 32000 };
+	double legsX{ 100 }, legsTP{ 20 }, legsREP{ 50 }, legsTIME{ 16 };
+	//quest data
+
+	//input
+	long inputX{}, inputZL{};
+	cout << "How many legs did you buy: ";
+	cin >> inputX;
+	cout << "How much money for 1: ";
+	cin >> inputZL;
+	// start gross rewards
 	cout << "-----------------------------\n";
-	cout << "1[Big spider legs]\n";
-	cout << "2[Hog mutant knees]\n";
-	cout << "3[Shelled verliock paws]\n";
-	cout << "4[Peat spider legs]\n";
-	cout << "-----------------------------\n";
-	cout << "Select a numer 1-4: ";
-	//start of selection code
-	int category{};
-	cin >> category;
-	if (category == 1) {
-		calculateLegs();
+	cout << "You will get the following \n";
+
+
+	long zl = (legsZL / legsX) * inputX;//gross revenue
+	long netzl = zl - (inputX * inputZL);//revenue minus cost of legs
+
+	long tp = (legsTP / legsX) * inputX;//trade points
+	int tools = tp / 200;// how many old tools can be bought with trade points
+
+	long total=netzl;
+
+	if (tools > 0) { // if you can, adds revenue from tools to total profits
+	    total = (tools*391027)+netzl;
+	}
+	
+	long xp = (legsXP / legsX) * inputX;
+	cout << "Experience: " << xp << '\n';;
+
+	long rep = (legsREP / legsX) * inputX;
+	cout << "Reputation: " << rep << '\n';
+
+	if (tp >= 200) { //if enough for a tool, mention it
+		cout << "Trade points: " << tp << ", which is enough for " << tools << " old tool(s)" <<'\n';
+	}
+	else {
+		cout << "Trade points: " << tp << '\n';
 	}
 
+	cout << "Profits: " << netzl << '\n';
 	
-	//Q=quantity, ZL=money XP=experience TP=trade points REP= reputation, Time = quest refresh in hours
-	float kneeQ{ 15 } , kneeZL{ 90000 }, kneeXP{ 86000 }, kneeTP{ 40 }, kneeREP{ 80 }, kneeTIME{ 12 };
-	float shellQ{ 3 }, shellZL{ 60000 }, shellXP{ 70000 }, shellTP{ 50 }, shellREP{ 100 }, shellTIME{ 8 };
-	float peatQ{ 8 },   peatZL{   0   }, peatXP{ 250000 }, peatTP{ 300 }, peatREP{ 500 }, peatTIME{ 48 };
-	// end of dataset
-	
-	
-	
+	if (tools>0) { //if enough for a tool, add them to the total profits
+		cout << "Profits + old tools: " << total << '\n';
+	}
+
+
 
 }
